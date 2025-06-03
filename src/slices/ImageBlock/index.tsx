58bbfddf5060a89d461d9cfaaf45492fe98d4b25
Pierre <pierrestordiau980@gmail.com>
2025-06-03 23:58:15 +0200

    added missing components

---
+++ src/slices/ImageBlock/index.tsx
@@ -0,0 +1,21 @@
+import { FC } from "react";
+import { Content } from "@prismicio/client";
+import { SliceComponentProps } from "@prismicio/react";
+import {PrismicNextImage} from "@prismicio/next";
+/**
+ * Props for `ImageBlock`.
+ */
+export type ImageBlockProps = SliceComponentProps<Content.ImageBlockSlice>;
+
+/**
+ * Component for "ImageBlock" Slices.
+ */
+const ImageBlock: FC<ImageBlockProps> = ({ slice }) => {
+  return (
+      <div className="flex justify-center items-center">
+   <PrismicNextImage field={slice.primary.image} className=" flex max-w-250 rounded-lg items-center justify-center"/>
+      </div>
+  );
+};
+
+export default ImageBlock;
